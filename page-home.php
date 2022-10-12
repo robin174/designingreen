75aab7be67f59db0f59843c0a652ea73a14959d8
Robin Green <robin@designingreen.co.uk>
2022-10-12 17:16:03 +0100

    First commit

---
+++ page-home.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* Template Name: Page - Home
+* @package designingreen
+*/
+
+get_header(); ?>
+
+<section class="mol--page-main" style="padding: 100px 0;">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-md-9">
+				
+				<h1><?php the_title();?></h1>
+				<p>How do we update the navigation - we need 2x separate version.</p>
+				<!-- A transparent white block / full width at 50-70% would work well on the background -->
+				<a class="button atm--button-main" href="/labs">
+					<span>Discover more</span><i class="fas fa-arrow-right"></i>
+				</a>
+
+				<h2>How I can help you?</h2>
+				<p>Over the past 2 years I have started to hone the services I offer. Whether it's the opportunity to run an audit on your design or digital offering, or a full program of coaching, I am confident I can help you and I can help your business.</p>
+
+				<h2>Who is this for?</h2>
+
+				
+
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
---
+++ page-profile.php
@@ -0,0 +1,99 @@
+<?php
+/**
+* Template Name: Page - Profile
+* @package designingreen
+*/
+
+get_header(); ?>
+
+<section class="org--profile-header" style="background-color: rgba(<?php the_field('profile_header-color'); ?>);">
+	<div class="container-fluid">
+		<div class="mol--profile-hero d-flex justify-content-center">
+			<figure>
+				<img class="img-fluid" src="<?php the_field('profile_header-image'); ?>" alt="hero image">
+			</figure>
+		</div>
+	</div>
+</section>
+
+<section class="org--profile-columns">
+	<div class="container">
+		<div class="row justify-content-center">
+
+			<div class="col-12 col-md-12 col-lg-10">
+				<div class="row justify-content-center">
+					<div class="col-10 col-md-4">
+						<div class="mol--profile-secondary">
+							<div class="atm--profile-headshot">
+								<figure class="atm--headshot">
+									<img class="img-fluid" src="<?php the_field('profile_headshot'); ?>" alt="<?php the_field('profile_name'); ?>">
+								</figure>
+							</div>
+							<div class="atm--profile-blockquote">
+								<blockquote>
+									&ldquo;Technology and design. Two fundamentals of modern business today. Both broad topics that can feel overwhelming at first. integer lacinia nisi nec luctus aliquet. Integer sed quam nisi. In non felis nisi. Mauris sagittis euismod faucibus. Sed vestibulum enim at fringilla tempus.&rdquo;
+								</blockquote>
+							</div>
+						</div>
+					</div>
+					<div class="col-10 col-md-8 col-lg-7 offset-lg-1">
+						<div class="mol--profile-primary">
+							<h2 class="atm--profile-title"><?php the_field('profile_name'); ?></h2>
+							<p class="atm--profile-details"><a href="mailto:robin@designingreen.co.uk" title="Email <?php the_field('profile_name'); ?>"><?php the_field('profile_email'); ?></a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://www.linkedin.com/in/robingreen174/" title="<?php the_field('profile_name'); ?> on LinkedIn" target="_blank"><i class="fab fa-linkedin fa-lg"></i></a></p>
+							<div class="atm--profile-location">
+								<h5 class="ut--uc">Location</h5>
+								<h4><?php the_field('profile_location'); ?></h4>
+							</div>
+							<div class="atm--profile-description">
+								<p>I am many things to many businesses. I am a designer, developer and creative technologist. Learning to code was a fundamental step in facilitating a move from traditional graphic design into digital design early on in my career.</p>
+								<p>Fascinated by how each and every one of us interacts with technology and design differently, Robin started delivering workshops on all aspects of the digital spectrum 9 years ago. ... ... Current areas of interest include internet of things, blockchain, security and future technologies.</p>
+							</div>
+							<div class="atm--profile-entities">
+								<h5 class="ut--uc">Live projects</h5>
+								<div class="mol--entity-wrapper d-flex">
+									<div class="atm--entity-logo d-flex align-items-center justify-content-center">
+										<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-greenhouse-v2.svg" alt="robingreen.house logo">
+									</div>
+									<div class="atm--entity-details flex-grow-1">
+										<span class="d-block atm--entity-title">robingreen.house</span>
+										<span class="d-block atm--entity-desc">Innovation as a Service</span>
+									</div>
+									<div class="atm--entity-arrow">
+										<a class="button atm--button-square-i d-flex align-items-center justify-content-center" href="https://robingreen.house" target="_blank"><i class="fas fa-lg fa-arrow-right"></i></a>
+									</div>
+								</div>
+								<div class="mol--entity-wrapper d-flex">
+									<div class="atm--entity-logo d-flex align-items-center justify-content-center">
+										<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-greenhouse-v2.svg" alt="robingreen.house logo">
+									</div>
+									<div class="atm--entity-details flex-grow-1">
+										<span class="d-block atm--entity-title">rei•iot</span>
+										<span class="d-block atm--entity-desc">Description</span>
+									</div>
+									<div class="atm--entity-arrow">
+										<a class="button atm--button-square-i d-flex align-items-center justify-content-center" href="https://rei-iot.com/" target="_blank"><i class="fas fa-lg fa-arrow-right"></i></a>
+									</div>
+								</div>
+								<div class="mol--entity-wrapper d-flex">
+									<div class="atm--entity-logo d-flex align-items-center justify-content-center">
+										<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-greenhouse-v2.svg" alt="robingreen.house logo">
+									</div>
+									<div class="atm--entity-details flex-grow-1">
+										<span class="d-block atm--entity-title">Greenfinch</span>
+										<span class="d-block atm--entity-desc">Decentralized Storage</span>
+									</div>
+									<div class="atm--entity-arrow">
+										<a class="button atm--button-square-i d-flex align-items-center justify-content-center" href="https://greenfinch.app" target="_blank"><i class="fas fa-lg fa-arrow-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
